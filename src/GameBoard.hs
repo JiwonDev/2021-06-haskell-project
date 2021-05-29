cd32fed12b519ebcf0b61f5f073930aeb5b5caf9
JiwonDev <sylvester7412@gmail.com>
2021-05-30 01:16:09 +0900

    GameBoard.hs 작성 스크린 사이즈를 이용하여 block, grid(게임판), screen 관련 함수 정의

---
+++ src/GameBoard.hs
@@ -0,0 +1,33 @@
+module GameBoard where
+
+import Control.Monad
+
+-- 그리드 가로 사이즈 (개수)
+gridWidth :: Num a => a
+gridWidth = 30
+
+-- 그리드 세로 사이즈
+gridHeight :: Num a => a
+gridHeight = 30
+
+-- 만약 전체화면(fullScreen) 으로 실행하고싶다면
+-- Graphics.Gloss.Interface.Environment 에 있는 getScreenSize 이용
+-- fst getScreenSize, snd getScreenSize
+getScreenSize :: IO (Int, Int)
+getScreenSize = return (800,  800)
+
+-- 픽셀의 가로 갯수 (스크린 내에서)
+screenWidth :: Num a => IO a
+screenWidth = fromIntegral . fst <$> getScreenSize
+
+-- 픽셀의 세로 갯수 (스크린 내에서)
+screenHeight :: Num a => IO a
+screenHeight = fromIntegral . snd <$> getScreenSize
+
+-- 총 가로 블럭 갯수, 블럭크기 / 스크린 사이즈
+blockWidth :: IO Float
+blockWidth = (/ (gridWidth + 1)) <$> screenWidth
+
+-- 총 세로 블럭 갯수
+blockHeight :: IO Float
+blockHeight = (/ (gridHeight + 1)) <$> screenHeight
